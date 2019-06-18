78a5176aaa36d6640395b130257cfd68eee42152
fnuzzo <fnuzzo@kth.se>
2019-06-18 16:23:14 +0200

    Code cleanup

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
-import argparse
-
 from flir_image_extractor import FlirImageExtractor
 from image_processing import ImageProcessor, StreamProcessor
 
-def main_thermal_conversion():
-    parser = argparse.ArgumentParser(description='Extract and visualize Flir Image data')
-    parser.add_argument('-i', '--input', type=str, help='Input image. Ex. img.jpg', required=True)
-    parser.add_argument('-p', '--plot', help='Generate a plot using matplotlib', required=False, action='store_true')
-    parser.add_argument('-exif', '--exiftool', type=str, help='Custom path to exiftool', required=False,
-                        default='exiftool')
-    parser.add_argument('-csv', '--extractcsv', help='Export the thermal data per pixel encoded as csv file',
-                        required=False)
-    parser.add_argument('-d', '--debug', help='Set the debug flag', required=False,
-                        action='store_true')
-    args = parser.parse_args()
-
-    fie = FlirImageExtractor(exiftool_path=args.exiftool, is_debug=args.debug)
-    fie.process_image(args.input)
-
-    if args.plot:
-        fie.plot()
-
-    if args.extractcsv:
-        fie.export_thermal_to_csv(args.extractcsv)
-
-    fie.save_images()
-
 
 def main_webcam_stream():
-
-    image_processor = ImageProcessor(option='faces_and_eyes')
+    image_processor = ImageProcessor(option='better_faces_and_eyes')
     stream_processor = StreamProcessor(image_processor, index_cam=0)
 
     stream_processor.run()
 
 
+def main_flir_image_processor():
+    input_file = 'test_images/flir_20190618T092717.jpg'
+    file_name = input_file.split('/')[1].split('.')[0]
+
+    fie = FlirImageExtractor()
+    fie.process_image(input_file)
+
+    # fie.plot()
+
+    fie.export_thermal_to_csv('thermals_csv/'+file_name+'_thermal_csv.csv')
+
+    fie.save_images()
+
+
+
+def main_flir_video_processor():
+    pass
+
+
 if __name__ == '__main__':
-    main_webcam_stream()
+    # main_webcam_stream()
+    main_flir_image_processor()
