c46f54c682c892eea13d2f6f66cb67c4e3a5e516
fnuzzo <fnuzzo@kth.se>
2019-07-10 11:31:15 +0200

    Listen to client and show received images

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import tiffcapture as tc
 
 def main_webcam_stream():
     image_processor = ImageProcessor(option='dlib_68landmarks')
-    stream_processor = StreamProcessor(image_processor, index_cam=0)
+    stream_processor = StreamProcessor(image_processor, index_cam=2)
 
     stream_processor.run()
 
@@ -23,8 +23,9 @@ def main_flir_image_processor():
     flir_20190619T161858.jpg
     flir_20190620T113530.jpg (horizontal)
     flir_20190620T113555.jpg (horizontal)
+    FLIROne-2019-07-09-14-47-31+0200.jpg
     """
-    input_file = 'test_images/flir_20190620T113530.jpg'
+    input_file = 'test_images/' + 'FLIROne-2019-07-09-14-47-31+0200.jpg'
 
     fie = FlirImageExtractor()
     fie.process_image(input_file, upsample_thermal=True, transform_rgb=True)
@@ -74,7 +75,7 @@ def main_flir_video_processor():
     count = 0
     # Read until video is completed
     while cap.isOpened():
-        cap.set(cv2.CAP_PROP_CONVERT_RGB, cv2.CV_16U)
+        # cap.set(cv2.CAP_PROP_CONVERT_RGB, cv2.CV_16U)
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret is True:
@@ -129,7 +130,61 @@ def main_flir_video_processor():
     cv2.destroyAllWindows()
 
 
+def main_server_flir_app():
+    import socket
+    import numpy
+
+    def recvall(sock, count):
+        buf = b''
+        while count:
+            newbuf = sock.recv(count)
+            if not newbuf: return None
+            buf += newbuf
+            count -= len(newbuf)
+        return buf
+
+    TCP_IP = socket.gethostname()
+    TCP_PORT = 1234
+    server_address = (TCP_IP, TCP_PORT)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.bind(server_address)
+    except Exception as ex:
+        print("Not connected: " + str(ex))
+
+    print(s)
+    s.listen(True)
+    s.settimeout(60)
+
+    stopped = False
+    while not stopped:
+        try:
+            conn, (ip, port) = s.accept()
+
+            length = recvall(conn, 4)
+            if length is None:
+                continue
+            length = int.from_bytes(length, "big")
+            if not length > 0:
+                continue
+            stringData = recvall(conn, length)
+            # data = numpy.fromstring(stringData, dtype='uint8')
+            data = numpy.frombuffer(stringData, dtype='uint8')
+
+            decimg = cv2.imdecode(data, 1)
+            cv2.imshow('SERVER', decimg)
+            cv2.waitKey(1)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+        except socket.timeout:
+            print("Socket timed out")
+            stopped = True
+    s.close()
+
+
 if __name__ == '__main__':
     # main_webcam_stream()
-    main_flir_image_processor()
+    # main_flir_image_processor()
     # main_flir_video_processor()
+    main_server_flir_app()
